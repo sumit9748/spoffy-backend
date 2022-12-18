065bdc635ed0c586c71eed16b5233722b854031d
sumit <sumitmondal9448@gmail.com>
2022-12-18 23:44:32 +0530

    bug

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.use("/connect/checkout", stripeRoute);
 
 if (process.env.NODE_ENV == "production") {
   app.use(express.static("spoffy/build"));
-  const path = required("path");
+  const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resole(__dirname, "spoffy", "build", "index.html"));
   });
